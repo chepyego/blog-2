22be6742e4bdd35c2587d39cbad9d7ff8a28e421
chepyego <jchepyego@gmail.com>
2018-10-19 15:00:17 +0300

    added helper and finished styling with boostrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    #if active page color- white
+    #if not active page color = regular
+    #Take argurments: 'what page its on','url', change font color
+
+    def active_link_to(nav_link, contrl, url, style)
+        if controller_name == contrl && action_name == action
+            return link_to nav_link,url, id:style
+        else
+            return link_to nav_link, url, class:style
+        end
+    end
 end
